8321c358959b432d37f93eabbcd1b4ab4c3aea09
Christian <Christian.kento@gmail.com>
2020-05-17 16:42:14 -0500

    Updated documentation

--- Video_to_frame.py
+++ Video_to_frame.py
@@ -1,25 +1,35 @@
-from cv2 import cv2
+"""Script used to convert a video into each individual frame and saves them
+"""
+
 import argparse
+from pathlib import Path
+from cv2 import cv2
+
 
 def main():
+    """Converts a video into its individual frames and saves them
+    """
     parser = argparse.ArgumentParser(description='Set arguments for converting video to images')
-    parser.add_argument('video_dir', type = str, default='video.mp4', help='select directory for video')
-    parser.add_argument('--video_skip', type = int, default=1, help='select how many frames to skip')
+    parser.add_argument('video_in_dir', type=str, help='select directory for video')
+    parser.add_argument('--video_skip', type=int, default=1, help='select how many frames to skip')
+
     # gets our arguments from the command line
     in_arg = parser.parse_args()
-    
+
     # gets the video
-    vidcap = cv2.VideoCapture(in_arg.video_dir)
-    success,image = vidcap.read()
+    vidcap = cv2.VideoCapture(in_arg.video_in_dir)
+    success, image = vidcap.read()
     count = 0
-    
+
     while success:
         if count % in_arg.video_skip == 0:
-            cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-            success,image = vidcap.read()
+            cv2.imwrite("frame_{}.jpg".format(count), image)     # save frame as JPEG file
+            success, image = vidcap.read()
             print(f'Read a new frame {count}: ', success)
-        success,image = vidcap.read()
+
+        success, image = vidcap.read()
         count += 1
+
 
 # Call to main function to run the program
 if __name__ == "__main__":
