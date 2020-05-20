e94900d55485b8f4212c99c8818fe3fe38f6b4d7
Christian <Christian.kento@gmail.com>
2020-05-20 09:46:24 -0500

    Updated to include option to specify save folder

--- Video_to_frame.py
+++ Video_to_frame.py
@@ -11,10 +11,14 @@ def main():
     """
     parser = argparse.ArgumentParser(description='Set arguments for converting video to images')
     parser.add_argument('video_in_dir', type=str, help='select directory for video')
+    parser.add_argument('--save_dir', type=str, help='select directory for frames out')
     parser.add_argument('--video_skip', type=int, default=1, help='select how many frames to skip')
 
     # gets our arguments from the command line
     in_arg = parser.parse_args()
+
+    # creates the dir where the photos will be stored if it does not exist
+    Path(in_arg.save_dir).mkdir(parents=True, exist_ok=True)
 
     # gets the video
     vidcap = cv2.VideoCapture(in_arg.video_in_dir)
@@ -23,7 +27,7 @@ def main():
 
     while success:
         if count % in_arg.video_skip == 0:
-            cv2.imwrite("frame_{}.jpg".format(count), image)     # save frame as JPEG file
+            cv2.imwrite("{}/frame_{}.jpg".format(in_arg.save_dir, count), image)     # save frame as JPEG file
             success, image = vidcap.read()
             print(f'Read a new frame {count}: ', success)
 
